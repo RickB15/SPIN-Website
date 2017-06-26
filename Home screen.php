b4e0b60ca4494b12462430e31c996eb6df1cf5e4
RickB15 <rickblanksma@gmail.com>
2017-06-26 16:04:42 +0200

    Een beginetje
    Er moet nog wat gedaan worden aan de positie waar alles staat (icoontjes in het midden en video full screen) maar dat krijg ik niet voor elkaar

---
+++ Home screen.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>SPIN-Boardsports</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+  <div id="bg_container">
+    <video id="bg" src="C:\Users\Rick_B15\Desktop\Website Spin\SPIN_Promo.mp4" autoplay="true" loop="true" muted="true"</video>
+  </div>
+  <div id="icons_container">
+    <img id="Party" src="C:\Users\Rick_B15\Desktop\Website Spin\Party.png" alt="Party">
+    <img id="Kite" src="C:\Users\Rick_B15\Desktop\Website Spin\Kite.png" alt="Kite">
+    <img id="Skate" src="C:\Users\Rick_B15\Desktop\Website Spin\SKate.png" alt="Skate">
+    <img id="Snow" src="C:\Users\Rick_B15\Desktop\Website Spin\Snow.png" alt="Snow">
+    <img id="Surf" src="C:\Users\Rick_B15\Desktop\Website Spin\Surf.png" alt="Surf">
+    <img id="Wake" src="C:\Users\Rick_B15\Desktop\Website Spin\Wake.png" alt="Wake">
+  </div>
+</body>
+</html>
